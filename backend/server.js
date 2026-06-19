1b5c58940e1796cf2a026301b896b9a91cf365aa
Hounkpevi Olympe <olympe.hounkpevi@epitech.eu>
2026-06-19 10:07:37 +0100

    fix: comments

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,13 @@ const triviaGame = require('./game-trivia');
 const bots       = require('./game-bots');
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
